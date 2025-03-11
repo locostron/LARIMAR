5d8e3b0df33f68d7667aecf0f9b63bb0fd8561a3
locostron <54696842+locostron@users.noreply.github.com>
2025-03-10 21:29:28 -0400

    Implementar las transacciones

---
+++ src/transaction.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+
+class CTransaction {
+public:
+    std::string sender;
+    std::string receiver;
+    double amount;
+    
+    CTransaction(std::string sender, std::string receiver, double amount);
+    std::string GetTransactionHash() const;
+};
