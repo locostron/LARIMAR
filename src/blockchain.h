17860c9544abcd0745d9b276ddf4531a659cbd46
locostron <54696842+locostron@users.noreply.github.com>
2025-03-10 21:36:48 -0400

    Update blockchain.h

--- src/blockchain.h
+++ src/blockchain.h
@@ -1,13 +1,1 @@
-#pragma once
-
-#include <vector>
-#include "block.h"
-
-class Blockchain {
-public:
-    std::vector<CBlock> chain;
-
-    Blockchain();
-    void AddBlock(const CBlock& block);
-    CBlock GetLastBlock() const;
-};
+void AddTransactionToBlock(const CTransaction& tx);
