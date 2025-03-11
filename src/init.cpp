d008e58b5920aeea0e87c72c46e289666a082e25
locostron <54696842+locostron@users.noreply.github.com>
2025-03-10 21:20:03 -0400

    Delete src/init.cpp

--- src/init.cpp
+++
@@ -1,9 +0,0 @@
-#include "init.h"
-#include "chainparams.h"
-
-bool AppInit() {
-    // Inicia la cadena de bloques con los parámetros correctos
-    SelectParams(CBaseChainParams::MAIN);
-    // Otros ajustes para iniciar la red
-    return true;
-}
