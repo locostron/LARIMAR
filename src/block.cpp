edb8e58276222b51faea6016433601f464bede29
locostron <54696842+locostron@users.noreply.github.com>
2025-03-10 20:42:17 -0400

    Update block.cpp

--- src/block.cpp
+++ src/block.cpp
@@ -1,10 +1,21 @@
 #include "block.h"
-#include "validation.h"
+#include <iostream>
+#include <sstream>
+#include <openssl/sha.h>
+
+CBlock::CBlock(int height, std::string prevHash)
+    : nHeight(height), prevBlockHash(prevHash), nTime(time(nullptr)), nNonce(0) {}
+
+std::string CBlock::GetHash() const {
+    std::stringstream ss;
+    ss << nHeight << prevBlockHash << nTime << nNonce;
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256((unsigned char*)ss.str().c_str(), ss.str().size(), hash);
+    std::stringstream hashString;
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+        hashString << std::hex << (int)hash[i];
+    return hashString.str();
+}
 
 void CBlock::AddTransaction(const CTransaction& tx) {
-    if (TransactionValidator::IsValid(tx)) {
-        vTransactions.push_back(tx);
-    } else {
-        std::cerr << "Transacción rechazada." << std::endl;
-    }
-}
+    vTransactions.push_back(tx);
