041a4e90ccb09b95c28e634964232cc20f0d65d0
locostron <54696842+locostron@users.noreply.github.com>
2025-03-10 21:41:50 -0400

    Update blockchain.h

--- src/blockchain.h
+++ src/blockchain.h
@@ -1,1 +1,12 @@
-void AddTransactionToBlock(const CTransaction& tx);
+#include "network.h"
+
+class Blockchain {
+public:
+    std::vector<CBlock> chain;
+    P2PNetwork network;  // Agregar la red P2P
+
+    Blockchain();
+    void AddBlock(const CBlock& block);
+    void AddTransactionToBlock(const CTransaction& tx);
+    void SyncBlockchain();
+};
